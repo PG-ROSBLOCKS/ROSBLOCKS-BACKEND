ec5697aa043a23338d04218e274501d97c75dd0a
Kevin Estiven Velasco Pinto <kevinevelasco@javeriana.edu.co>
2025-03-07 19:22:28 -0500

    se recibe el client (falta corregir errores desde el front)

--- Back-End/app/routers/upload.py
+++ Back-End/app/routers/upload.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from models.requests import UploadRequest
-from services.code_generator import generate_pub_sub, generate_service, generate_message, generate_server
+from services.code_generator import generate_pub_sub, generate_service, generate_message, generate_server, generate_client
 import logging
 
 router = APIRouter()
@@ -17,5 +17,7 @@ async def upload_code(request: UploadRequest):
         return await generate_message(request)
     elif request.type.startswith("server"):
         return await generate_server(request)
+    elif request.type.startswith("client"):
+        return await generate_client(request)
     else:
         raise HTTPException(status_code=400, detail="Tipo de request no soportado")
