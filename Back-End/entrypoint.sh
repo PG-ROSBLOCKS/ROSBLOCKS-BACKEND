71eb2f4d4bd64e84bf50d0313a9d94c844b3fb6d
Juan Andres <juanandresc.jarp@gmail.com>
2025-04-04 19:45:23 -0500

    not tested

--- Back-End/entrypoint.sh
+++ Back-End/entrypoint.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
-set -e  # Hace que el script termine si un comando falla
+set -e
 
-# Fuente del entorno ROS
-source /opt/ros/jazzy/setup.bash
+# Establecer display virtual
+export DISPLAY=:99
+export XDG_RUNTIME_DIR=/tmp/runtime-root
+mkdir -p $XDG_RUNTIME_DIR
 
-# Iniciar FastAPI en segundo plano
-/opt/venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 &
+echo "[INFO] Iniciando Xvfb..."
+Xvfb $DISPLAY -screen 0 1024x768x24 &
+sleep 2
+openbox > /dev/null 2>&1 &
 
-# Esperar unos segundos para asegurar que FastAPI inicia correctamente
-sleep 5
 
-# Iniciar Turtlesim en primer plano
+echo "[INFO] Iniciando x11vnc..."
+x11vnc -display $DISPLAY -forever -nopw -shared -rfbport 5900 -bg
+
+echo "[INFO] Iniciando websockify + noVNC..."
+mkdir -p /opt/novnc
+cp -r /usr/share/novnc/* /opt/novnc/
+cd /opt/novnc
+
+# Esto sirve la interfaz web de noVNC en el puerto 8080 y la conecta al VNC local
+websockify --web . --wrap-mode=ignore 8080 localhost:5900 &
+
+echo "[INFO] Iniciando backend FastAPI..."
+cd /app
+exec /opt/venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000
+
+
+# Pequeño delay opcional
+sleep 2
+
+# Lanzar turtlesim en primer plano
 exec ros2 run turtlesim turtlesim_node
