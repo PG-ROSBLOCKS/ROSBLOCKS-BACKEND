86f40b97a136c17127374f0dc51d520f891596a5
kevinevelasco <90013072+kevinevelasco@users.noreply.github.com>
2025-04-26 17:23:51 -0500

    Update config.py

--- Back-End/app/config.py
+++ Back-End/app/config.py
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
     MSG_DIR: str = "/ros2_ws/src/sample_interfaces/msg"
     LOG_DIR: str = "/ros2_ws/logs"
     CMAKE_FILE: str = "/ros2_ws/src/sample_interfaces/CMakeLists.txt"
-    SESSION_MANAGER_URL: str= "http://54.147.32.204/api"
+    SESSION_MANAGER_URL: str= "http://44.223.14.250/api"
 
     class Config:
         env_file = ".env"
