65131d419ffc84fb4f268a9654a513697ec71bf5
Juan Andres Ramirez <juanandresc.jarp@gmail.com>
2025-05-15 22:54:53 -0500

    testing

--- Back-End/app/routers/ros_commands.py
+++ Back-End/app/routers/ros_commands.py
@@ -14,3 +14,12 @@ async def reset_container():
     )
     logging.info(f"ros2 topic list result:\n{result.stdout}")
     return {"message": "ROS2 TOPIC LIST EXECUTED", "output": result.stdout}
+
+@router.get("stop_ros_daemon", response_model=dict)
+async def stop_ros_daemon():
+    result = subprocess.run(
+        "bash -c 'source /ros2_ws/install/setup.bash && cd /ros2_ws && ros2 daemon stop'",
+        shell=True, check=False, capture_output=True, text=True
+    )
+    logging.info(f"ros2 daemon stop result:\n{result.stdout}")
+    return {"message": "ROS2 DAEMON STOP EXECUTED", "output": result.stdout}
